15da7f5b3928638dfb1df3e766b444c0fea13a14
Matthias Hopf <mhopf@suse.de>
2010-09-06 14:59:29 +0200

    Write out version to /etc/ImageVersion for so-far broken minor updates.

--- root/inst/netimginst.sh
+++ root/inst/netimginst.sh
@@ -3,6 +3,7 @@
 # Additional scripts (until next major update)
 test ! -e /inst/dia_gauge -a -e /read-write/inst/dia_gauge && ln -snf /read-write/inst/dia_gauge /inst/dia_gauge
 test -d /read-write && ln -snf /read-write/inst/selfupdate.sh /inst/selfupdate.sh
+echo "Network_Image_Installer-2.2.3" >/etc/ImageVersion
 
 /inst/network.sh
 /inst/checkupdate.sh
