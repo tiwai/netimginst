c9ce33344b82388d87114cf9e5eff835a4b565bf
Matthias Hopf <mhopf@suse.de>
2010-09-06 14:57:52 +0200

    Make minor self-updates persistent on USB sticks.

--- root/inst/selfminorupdate.sh
+++ root/inst/selfminorupdate.sh
@@ -9,7 +9,10 @@ shift
 vers="$1"
 file="$2"
 
-tar -C / -xvf "$file" || exit 1
+root=/
+test -d /read-write && root=/read-write/
+
+tar -C $root -xvf "$file" || exit 1
 
 sed -i -e 's/-[.0-9]*$/-'"$vers/" /etc/ImageVersion
 
