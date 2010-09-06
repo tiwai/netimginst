b213058f72b4c2b416ef2dd1c38f8c6d60ffe746
Matthias Hopf <mhopf@suse.de>
2010-09-06 18:11:13 +0200

    Bump to 2.2.5. Update Changelog. Update netimginst.sh

--- root/inst/netimginst.sh
+++ root/inst/netimginst.sh
@@ -6,7 +6,7 @@ test -d /read-write || rm /inst/selfupdate.sh
 # Additional scripts (until next major update)
 test ! -e /inst/dia_gauge -a -e /read-write/inst/dia_gauge && ln -snf /read-write/inst/dia_gauge /inst/dia_gauge
 test -d /read-write && ln -snf /read-write/inst/selfupdate.sh /inst/selfupdate.sh
-echo "Network_Image_Installer-2.2.4" >/etc/ImageVersion
+echo "Network_Image_Installer-2.2.5" >/etc/ImageVersion
 
 /inst/network.sh
 /inst/checkupdate.sh
