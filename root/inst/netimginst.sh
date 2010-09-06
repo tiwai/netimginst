15c33fc40cd7645b481a9b441060b8a489b13036
Matthias Hopf <mhopf@suse.de>
2010-09-06 14:57:05 +0200

    Add missing symlink to selfupdate.sh on 2.2.0 images.

--- root/inst/netimginst.sh
+++ root/inst/netimginst.sh
@@ -2,6 +2,7 @@
 
 # Additional scripts (until next major update)
 test ! -e /inst/dia_gauge -a -e /read-write/inst/dia_gauge && ln -snf /read-write/inst/dia_gauge /inst/dia_gauge
+test -d /read-write && ln -snf /read-write/inst/selfupdate.sh /inst/selfupdate.sh
 
 /inst/network.sh
 /inst/checkupdate.sh
