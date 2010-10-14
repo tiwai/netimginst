34c54cd3ba605302a449dbd2b3a4c23ce7e1332a
Matthias Hopf <mhopf@suse.de>
2010-10-14 14:24:02 +0200

    Nuke workarounds for selfupdate.

--- root/inst/netimginst.sh
+++ root/inst/netimginst.sh
@@ -3,11 +3,6 @@
 # Remove updater script if no persistent r/w area available (CDs)
 test -d /read-write || rm -f /inst/selfupdate.sh
 
-# Additional scripts (until next major update)
-test ! -e /inst/dia_gauge -a -e /read-write/inst/dia_gauge && ln -snf /read-write/inst/dia_gauge /inst/dia_gauge
-test -d /read-write && ln -snf /read-write/inst/selfupdate.sh /inst/selfupdate.sh
-echo "Network_Image_Installer-2.2.6" >/etc/ImageVersion
-
 /inst/network.sh
 /inst/checkupdate.sh
 exec /inst/installer.sh
