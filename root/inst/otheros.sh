af760153e05d5c5ef500e747fb3fe44cceb3c6c6
Matthias Hopf <mhopf@suse.de>
2010-10-21 17:05:05 +0200

    Fix otherOS installation on iso images. Also fail more gracefully.

--- root/inst/otheros.sh
+++ root/inst/otheros.sh
@@ -14,9 +14,14 @@ fi
 test $res = 0 || cp -a /boot/grub /mnt/boot/
 
 mount --bind /mnt/boot /boot || exit 1
+# /boot/grub/menu.lst not available on iso disks...
+touch /boot/grub/menu.lst
 /inst/setupgrubfornfsinstall ; res=$?
 
-test $res = 0 && eval "/bin/sh -x /boot/*/kexec"
+if [ $res = 0 ] ; then
+  eval "/bin/sh -x /boot/*/kexec"
+  res=$?
+fi
 umount /boot
 test $res = 0  && echo >$cmdfile "exec /inst/bootstrap.sh /inst/kexec.sh"
 
